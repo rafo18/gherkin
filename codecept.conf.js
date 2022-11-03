f49f80c5254708b3f8f7678dbe6f4089f9e9a371
Rafael Barreto <rafaelbarreto10@hotmail.com>
2022-11-03 10:56:49 -0400

    first commit

---
+++ codecept.conf.js
@@ -0,0 +1,52 @@
+exports.config = {
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'https://platzi.com',
+      show: true,
+      windowSize: '1200x900'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  mocha: {},
+  bootstrap: null,
+  timeout: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/steps.js']
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    retryFailedStep: {
+      enabled: true
+    },
+    retryTo: {
+      enabled: true
+    },
+    eachElement: {
+      enabled: true
+    },
+    pauseOnFail: {}
+  },
+  stepTimeout: 0,
+  stepTimeoutOverride: [{
+      pattern: 'wait.*',
+      timeout: 0
+    },
+    {
+      pattern: 'amOnPage',
+      timeout: 0
+    }
+  ],
+  tests: 'y',
+  name: '4-gherkin'
+}
